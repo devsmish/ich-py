41639e71b388dc59b4207cf4fc802e89e8c76ffb
Serhii <smyshynskyi@gmail.com>
2025-10-20 21:53:06 +0200

    add class work

---
+++ L1. Print and Input/CW.py
@@ -0,0 +1,11 @@
+from pyexpat.errors import messages
+
+first_name = input ("Enter your name: ")
+age = input ("Enter your age: ")
+town = input ("Enter your town: ")
+print (first_name + " " + age + " лет, проживает в городе " + town)
+
+message = input ("Enter your message: ")
+print (message)
+print (message)
+print (message)
