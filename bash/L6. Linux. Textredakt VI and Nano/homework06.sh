c27c49afa30c32db57d72673ca35c01a080d42d3
Serhii <smyshynskyi@gmail.com>
2025-11-06 23:44:14 +0100

    add homework06.sh

---
+++ bash/L6. Linux. Textredakt VI and Nano/homework06.sh
@@ -0,0 +1,12 @@
+   0 date >> /home/homework
+   1 df -h / >> /home/homework 
+   2 cat /home/homework 
+   3 history | tail -n 3 >> /home/homework 
+   4 cat /etc/passwd | grep cron >> /home/homework 
+   5 grep -c root /etc/group | sed 's/Строк с root: /' >> /home/homework 
+   6 grep -c root /etc/group | wc -l >> /home/homework 
+   7 cat /home/homework 
+   8 vi /home/homework 
+   9 cat /home/homework 
+  10 history >> homework06.txt && export_file homework06.txt
+  11 history >> homework06.sh && export_file homework06.sh
