8736ed474206ed654689ed2f777a2c1dc0e2785c
Serhii <smyshynskyi@gmail.com>
2025-11-16 19:31:37 +0100

    update classwork04.sql Create table and View

--- mySQL/04. Создание и изменение таблиц. View/classwork04.sql
+++ mySQL/04. Создание и изменение таблиц. View/classwork04.sql
@@ -47,6 +47,14 @@ VALUES
     ('Iv', 'Rocher', '1995-02-02', 4700.00, 'ivro@outlook.com'),
     ('Arno', 'Lehmann', '1990-03-03', 2500.00, 'lehmar@yahoo.com');
     
+# добавление тестовых данных
+INSERT INTO Employees (FirstName, LastName, BirthDate, Salary, Email)
+VALUES ('Alice', 'Green', '1985-05-15', 55000.00, 'alice.green@example.com'),
+ ('Bob', 'Smith', '1990-08-22', 60000.00, 'bob.smith@example.com'),
+('Charlie', 'Johnson', '1988-02-10', 52000.00, 'charlie.johnson@example.com'),
+('Diana', 'Williams', '1992-11-01', 58000.00, 'diana.williams@example.com'),
+('Edward', 'Brown', '1987-09-30', 61000.00, 'edward.brown@example.com');
+    
 # удаление записей
 delete from Employees
 where EmployeedID = 5;
@@ -58,13 +66,31 @@ DELETE FROM Employees;
 # очистка данных в таблице
 TRUNCATE TABLE Employees;
 
-# создание копии таблицыALTER
-
-# обновление данных в таблице
+# Изменить зарплату сотрудника с EmployeeID = 1 на 65000
 UPDATE Employees
 SET Salary = 65000
 WHERE EmployeeID = 1;
 
+# Увеличить зарплату всех сотрудников, работающих с 2024 года, на 10%
 UPDATE Employees
 SET Salary = Salary * 1.1
 WHERE YEAR(HireDate) >= '2024';
+
+# Создайте таблицу с первыми двумя строками таблицы Employees.
+CREATE TABLE Employees_short AS
+SELECT * FROM Employees
+LIMIT 2;
+
+# VIEW
+# 1. Создайте представление на основе всех записей таблицы Employees в Вашей базе данных.
+# 2. Создайте представление выбрав первые два столбца из таблицы Employees в базе данных.
+# 3. Предварительно обновить схему данных.
+
+USE 101025_SerhiiM;
+
+CREATE VIEW firs_view AS
+SELECT * FROM Employees;
+
+CREATE VIEW second_view AS
+SELECT EmployeeID, FirstName
+FROM Employees
