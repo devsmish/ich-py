b2c91313800022c553daad63905aedbea6541b0f
devsmish <smyshynskyi@gmail.com>
2025-11-26 22:22:12 +0100

    update practice05.py

--- L21. Practicum_05/practice_05.py
+++ L21. Practicum_05/practice_05.py
@@ -62,7 +62,35 @@ for i in range(len(num_list)):
     result += int(num_list[i])
 print(result)
 
-'''Задание
+'''2. Обратный список
+Задача: Напишите программу, которая принимает строку с числами через пробел и
+выводит его в обратном порядке. Обратите внимание, что в списке должны быть
+именно числа, а не строки.
+Пример вывода: Введите список чисел через пробел: 1 2 3 4 5
+Обратный список: [5, 4, 3, 2, 1]'''
+nummers = input("Enter any nummer sep by space: ")
+list_nummers = nummers.split()
+for i in range(len(list_nummers)):
+    list_nummers[i] = int(list_nummers[i])
+print("Обратный список:", list_nummers[::-1])
+
+'''3. Сумма чётных и нечётных чисел
+Задача: Напишите программу, которая подсчитывает сумму чётных и нечётных
+чисел в списке по отдельности.
+Данные: numbers = [-1, 8, 2, 0, -3, -9, -1, 10, 7, -4]
+Пример вывода: Изначальный список: [-1, 8, 2, 0, -3, -9, -1, 10, 7, -4] Сумма чётных: 16
+Сумма нечётных: -7'''
+numbers = [-1, 8, 2, 0, -3, -9, -1, 10, 7, -4]
+even = odd = 0
+for num in numbers:
+    if num % 2 == 0:
+        even += num
+    else:
+        odd += num
+print("Сумма чётных:", even)
+print("Сумма нечётных:", odd)
+
+'''4. Задание
 Максимальное число
 Напишите программу, которая выводит наибольшее число в списке, не используя встроенную функцию max(), а также его индекс в списке.
 numbers = [3, 12, 8, 22, 9, 25, 6, 23, 8, 7]
@@ -101,6 +129,23 @@ for i in range(0, len(list1)-1,2):
     list1[i], list1[i+1] = list1[i+1], list1[i]
 print(f"Список после перестановки:{list1}")
 
+'''6. Числа и слова
+Задача: Напишите программу, которая обрабатывает строку, содержащую слова и
+целые числа, разделённые пробелами. Программа должна преобразовать строку в
+список, где числа преобразуются в тип int, а остальные элементы остаются
+строками и начинаются с заглавной буквы.
+Данные: text = "apple 42 banana 3 100 orange"
+Пример вывода: Изначальная строка: apple 42 banana 3 100 orange Результат:
+['Apple', 42, 'Banana', 3, 100, 'Orange']'''
+text = "apple 42 banana 3 100 orange"
+text_list = text.split()
+for i in range(len(text_list)):
+    if text_list[i].isdigit():
+        text_list[i] = int(text_list[i])
+    else:
+        text_list[i] = text_list[i].title()
+print(text_list)
+
 # Задание 7
 '''Кодирование строки
 Напишите программу, которая принимает строку и кодирует её с помощью следующего правила: каждая последовательность 
@@ -110,15 +155,15 @@ print(f"Список после перестановки:{list1}")
 Закодированная строка: a4b2c1'''
 text = input("Enter any text: ")
 res_str = ''
-letter = ''
-count_let = 0
+count_let = 1
+
 for i in range(len(text)):
-    letter = text[i]
     if i + 1 < len(text) and text[i] == text[i + 1]:
         count_let += 1
     else:
+        res_str += text[i] + str(count_let)
         count_let = 1
-res_str += letter + str(count_let)
+
 print(res_str)
 
 # Another variant
@@ -137,9 +182,81 @@ for i in range(1, len(s)):
 result += letter + str(count)
 print(result)
 
+'''8. Проверка формата email
+Задача: Напишите программу, которая проверяет, начинается ли строка с буквы,
+содержит ли в себе символ @, и заканчивается ли на .com или .de.
+Пример вывода: Введите email: user@example.com Корректный формат? True'''
+email = input("Enter any email: ")
+if email[0].isalpha() and email.count('@') == 1 and (email.endswith('.de') or email.endswith('.com')):
+    print("Корректный формат? True")
+else:
+    print("Некорректный формат? False")
+
+'''9. Количество символов
+Задача: Напишите программу, которая подсчитывает количество букв, цифр и
+пробелов в строке. Также выведите количество остальных символов.
+Пример вывода: Введите строку: Python 3.12 is awesome!
+Буквы: 15 Цифры: 3 Пробелы: 3 Остальные символы: 2
+'''
+text = input("Введите строку: ")
+count_letter = count_digit = count_space = count_other = 0
+for char in text:
+    if "0" <= char <= "9":
+        count_digit += 1
+    elif "A" <= char <= "Z" or "a" <= char <= "z":
+        count_letter += 1
+    elif char == " ":
+        count_space += 1
+    else:
+        count_other += 1
+print("Букв:", count_letter)
+print("Цифр:", count_digit)
+print("Пробелы:", count_space)
+print("Остальных символов:", count_other)
+
 # Задание 10
 '''10. Развернуть слова
 Напишите программу, которая разворачивает каждое слово в строке, сохраняя их порядок.
 Пример вывода:
 Введите строку: Python is fun 
 Результат: nohtyP si nuf'''
+text = input("Enter any text: ")
+word_list = text.split()
+new_text = ''
+for item in word_list:
+        new_text += item[::-1] + ' '
+print(new_text)
+
+'''11. По одному пробелу
+Задача: Напишите программу, которая удаляет в строке лишние пробелы, оставляя
+только по одному пробелу между словами.
+Данные: text = " Hello, World! How are you? "
+Пример вывода: Строка: ' Hello, World! How are you? ' Результат: 'Hello, World! How
+are you?'''
+text = input("Enter any text: ")
+correkt_text = ''
+tmp_list = text.split()
+for i in range(len(tmp_list)):
+    if i == 0:
+        correkt_text += tmp_list[i].lstrip() + ' '
+    elif i == len(tmp_list) - 1:
+        correkt_text += tmp_list[i].rstrip() + ' '
+    else:
+        correkt_text += tmp_list[i] + ' '
+print(correkt_text)
+
+'''12. Все позиции подстроки
+Задача: Напишите программу, которая ищет подстроку в строке и выводит все
+индексы начала вхождения подстроки.
+Пример вывода: Строка: Banana bAnana baNAna Подстрока: ban Позиция: 0 Позиция:
+7 Позиция: 14'''
+text = input("Enter any text: ")
+little_str = input("Enter any text: ")
+position = 0
+tmp_text = ''
+i = 0
+while i < len(text):
+    position = text.find(little_str)
+    i = position + 1
+    print("Позиция:", position)
+
